acadbe43ece61e08abb00d2df956744af5e32fe9
stephane <stefb12@gmail.com>
2021-02-27 14:39:57 +0100

    remove unnecessary json module import

--- gmail_stats_sender.py
+++ gmail_stats_sender.py
@@ -4,7 +4,6 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-import json
 import datetime
 from collections import Counter
 
